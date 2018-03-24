7409b82778130a8c8d773dc49841456715dc4a03
sunjdk <sunjdk@users.noreply.github.com>
2018-03-25 00:20:00 +0800

    删除项目中非必要文件

--- data/runtime/Data/site_options.php
+++
@@ -1,1 +0,0 @@
-a:10:{s:9:"site_name";s:6:"twh.io";s:9:"site_host";s:14:"http://twh.io/";s:9:"site_root";s:0:"";s:8:"site_icp";s:0:"";s:16:"site_admin_email";s:17:"1620016116@qq.com";s:11:"site_tongji";s:0:"";s:14:"site_copyright";s:0:"";s:14:"site_seo_title";s:6:"twh.io";s:17:"site_seo_keywords";s:3:"twh";s:20:"site_seo_description";s:3:"twh";}
